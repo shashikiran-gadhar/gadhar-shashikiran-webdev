8fa831679fe59ecb21d961341f551ed7251c96e8
shashikirang <shashikirang@ccs.neu.edu>
2017-02-15 23:02:03 -0500

    added default
    added default

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -69,6 +69,9 @@
                 templateUrl: "views/widget/templates/widget-edit.view.client.html",
                 controller: "EditWidgetController",
                 controllerAs: "model"
+            })
+            .otherwise({
+                redirectTo: "/login"
             });
     }
 
